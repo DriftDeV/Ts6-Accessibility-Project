3be9131a8c7fb45176f86bd769dcede7ed146079
Dr1ft <luigicolucci003@icloud.com>
2026-01-26 14:18:44 +0000

    Scrapped Code

--- src/debug_teamspeak.py
+++
@@ -1,8 +0,0 @@
-from ts_master import launch_teamspeak, get_os_info
-
-if __name__ == "__main__" : 
-    os_info = get_os_info()
-    print(f"[DEBUG] Lanciato Teamspeak in modabilità debug ")
-    launch_teamspeak(os_info)
-    exit()
-
